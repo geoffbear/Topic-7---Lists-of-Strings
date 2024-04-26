f3d46bde5a8a351646c8473d61a3386edb117af7
Geoffrey Dolbear <339470494@GoTVDSB.ca>
2024-04-26 12:21:22 -0400

    chnages

--- Program.cs
+++ Program.cs
@@ -15,12 +15,16 @@
             {
                 Console.WriteLine("Welcome to Geoffrey's Glorious Garden");
                 Console.WriteLine();
+                Console.WriteLine("Here is your garden:");
+                for (int i = 0; i < veg.Count; i++)
+                    Console.WriteLine(i + ". " + veg[i]);
+                Console.WriteLine();
                 Console.WriteLine("1 -- Pick a vegetable using its numerical index");
                 Console.WriteLine("2 -- Pick a vegetable by its name");
                 Console.WriteLine("3 -- Search for a vegetable");
                 Console.WriteLine("4 -- Plant a vegetable");
                 Console.WriteLine("5 -- Sort the garden");
-                Console.WriteLine("6 -- Clear the garden");
+                Console.WriteLine("6 -- Clear the garden...and quit the program");
                 while (!Int32.TryParse(Console.ReadLine(), out option))
                     Console.WriteLine("Invalid Numeric Input.  Try again.");
                 if (option == 1)
@@ -51,12 +55,6 @@
 
                 else if (option == 3)
                 {
-                    Console.WriteLine("Here is your garden:");
-                    for (int i = 0; i < veg.Count; i++)
-                    {
-                        Console.WriteLine(i + ". " + veg[i]);
-
-                    }
                     Console.WriteLine();
                     Console.WriteLine("Press Enter to continue");
                     Console.ReadLine();
@@ -68,7 +66,6 @@
                     for (int i = 0; i < veg.Count; i++)
                     {
                         Console.WriteLine(i + ". " + veg[i]);
-
                     }
                     Console.WriteLine();
                     Console.WriteLine("Press Enter to continue");
@@ -77,11 +74,11 @@
 
                 else if (option == 5)
                 {
-                    Console.WriteLine("Here is your garden:");
+                    veg.Sort();
+                    Console.WriteLine("Here is your sorted garden:");
                     for (int i = 0; i < veg.Count; i++)
                     {
                         Console.WriteLine(i + ". " + veg[i]);
-
                     }
                     Console.WriteLine();
                     Console.WriteLine("Press Enter to continue");
@@ -90,17 +87,14 @@
 
                 else if (option == 6)
                 {
-                    Console.WriteLine("Here is your garden:");
+                    Console.WriteLine("Here is your final garden:");
                     for (int i = 0; i < veg.Count; i++)
-                    {
                         Console.WriteLine(i + ". " + veg[i]);
-
-                    }
-                    Console.WriteLine();
-                    Console.WriteLine("Press Enter to continue");
+                    
+                    Console.WriteLine("Press Enter to say goodbye :(");
                     Console.ReadLine();
+                    menu = false;
                 }
-
             }
         }
     }
